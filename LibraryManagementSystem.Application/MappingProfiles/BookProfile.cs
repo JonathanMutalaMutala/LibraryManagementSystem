fa7b1177f733643b614f684dd76b84839a66141b
Mutala Mutala <jonamut12.live@gmail.com>
2023-12-12 23:14:24 -0500

    Add profile

---
+++ LibraryManagementSystem.Application/MappingProfiles/BookProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using LibraryManagementSystem.Application.Features.Book.Queries.GetAllBooks;
+using LibraryManagementSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Application.MappingProfiles
+{
+    public  class BookProfile : Profile
+    {
+        public BookProfile()
+        {
+            CreateMap<BookDto,Book>().ReverseMap();
+        }
+        
+    }
+}
