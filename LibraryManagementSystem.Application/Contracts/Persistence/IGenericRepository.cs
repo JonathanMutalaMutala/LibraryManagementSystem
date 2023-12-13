6947242590084a1ef4182d0a1ec8eedd37460bfa
Mutala Mutala <jonamut12.live@gmail.com>
2023-12-12 22:36:26 -0500

    Mise en place de Application Layer

---
+++ LibraryManagementSystem.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,36 @@
+﻿using LibraryManagementSystem.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Application.Contracts.Persistence
+{
+    /// <summary>
+    /// Implementer les IGenericRepository 
+    /// </summary>
+    /// <typeparam name="T">Prend un Type de BaseEntity</typeparam>
+    public  interface IGenericRepository<T> where T : BaseEntity
+    {
+        /// <summary>
+        /// Permet de recuperer toutes les informations 
+        /// </summary>
+        /// <returns>Une liste du Type passer</returns>
+        Task<IReadOnlyList<T>> GetAllAsync();
+
+        /// <summary>
+        /// Permet de recuperer un type en lui passant son ID
+        /// </summary>
+        /// <param name="id">Id de l'entité</param>
+        /// <returns></returns>
+        Task<T> GetByIdAsync(int id); 
+
+        Task<T> CreateAsync (T entity);
+
+        Task UpdateAsync (T entity);
+
+        Task DeleteAsync (int id);
+        
+    }
+}
