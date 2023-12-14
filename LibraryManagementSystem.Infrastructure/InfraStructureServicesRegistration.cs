24ef5a86c29a974026e342e9de82e1ce8e6afa45
Mutala Mutala <jonamut12.live@gmail.com>
2023-12-14 17:43:25 -0500

    Ajout du projet Infrastructure

---
+++ LibraryManagementSystem.Infrastructure/InfraStructureServicesRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Infrastructure
+{
+    public static class InfraStructureServicesRegistration
+    {
+        public static IServiceCollection AddInfraStructureServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
