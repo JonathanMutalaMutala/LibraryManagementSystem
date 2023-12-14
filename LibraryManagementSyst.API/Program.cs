6e234e4aa6a6604eb2c01a1e4ca56e76488a891a
Mutala Mutala <jonamut12.live@gmail.com>
2023-12-14 18:43:49 -0500

    Changement de L'api

--- LibraryManagementSyst.API/Program.cs
+++ LibraryManagementSyst.API/Program.cs
@@ -7,9 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
-
-
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 
 // Others Services 
@@ -27,12 +27,14 @@ builder.Services.AddCors(options =>
 });
 
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 app.UseCors("all");
