39d987fd0291eab6c1fcdc576bc5e8b9d8815c7b
Mutala Mutala <jonamut12.live@gmail.com>
2023-12-14 17:44:39 -0500

    Ajout de la classe DbContext

---
+++ LibraryManagementSystem.Persistence/DatabaseContext/LibraryManagementSystemDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Persistence.DatabaseContext
+{
+    public  class LibraryManagementSystemDbContext : DbContext
+    {
+        private readonly DbContextOptions<LibraryManagementSystemDbContext> options;
+
+        public LibraryManagementSystemDbContext(DbContextOptions<LibraryManagementSystemDbContext> options) : base(options)
+        {
+            this.options = options;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(LibraryManagementSystemDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
