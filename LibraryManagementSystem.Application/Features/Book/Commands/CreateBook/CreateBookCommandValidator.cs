566ffd3ee66eb09f54c9ab8c7937523740791a7f
Mutala Mutala <jonamut12.live@gmail.com>
2023-12-14 17:34:06 -0500

    .

--- LibraryManagementSystem.Application/Features/Book/Commands/CreateBook/CreateBookCommandValidator.cs
+++ LibraryManagementSystem.Application/Features/Book/Commands/CreateBook/CreateBookCommandValidator.cs
@@ -17,7 +17,7 @@ namespace LibraryManagementSystem.Application.Features.Book.Commands.CreateBook
             RuleFor(p => p.Author)
                 .NotEmpty().WithMessage("{PropertyName} is required")
                 .NotNull()
-                .MaximumLength(10).WithMessage("{PropertyName} must be less than 10 characters"); 
+                .MaximumLength(50).WithMessage("{PropertyName} must be less than 10 characters"); 
 
             RuleFor(p => p.Title)
                 .NotEmpty().WithMessage("{PropertyName} is required")
