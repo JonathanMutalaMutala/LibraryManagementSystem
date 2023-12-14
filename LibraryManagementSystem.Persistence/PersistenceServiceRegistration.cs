9e35bb49f4ee670a3d693bf545e3a778d42b2902
Mutala Mutala <jonamut12.live@gmail.com>
2023-12-14 18:23:49 -0500

    Ajout des Services Necessaire

---
+++ LibraryManagementSystem.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions;
+using Microsoft.Extensions.Configuration;
+using LibraryManagementSystem.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using LibraryManagementSystem.Application.Contracts.Persistence;
+using LibraryManagementSystem.Persistence.Repositories;
+
+namespace LibraryManagementSystem.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        /// <summary>
+        /// Ce service sera ajouter dans le program.cs de la Web Api 
+        /// Ce service permettra d'enregister la dbContext pour permettre de faire les operations des Repository
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<LibraryManagementSystemDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("LibraryDatabaseConnectionString"));
+            });
+
+            services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+            services.AddScoped<IBookRepository, BookRepository>(); 
+
+            return services;
+        }
+    }
+}
---
+++ LibraryManagementSystem.Persistence/Repositories/BookRepository.cs
@@ -0,0 +1,19 @@
+﻿using LibraryManagementSystem.Application.Contracts.Persistence;
+using LibraryManagementSystem.Domain.Entities;
+using LibraryManagementSystem.Persistence.DatabaseContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Persistence.Repositories
+{
+    public class BookRepository : GenericRepository<Domain.Entities.Book>,IBookRepository
+    {
+        public BookRepository(LibraryManagementSystemDbContext context) : base(context)
+        {
+
+        }
+    }
+}
