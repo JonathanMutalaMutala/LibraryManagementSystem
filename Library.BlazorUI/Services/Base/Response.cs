62b6fd7b31c8e6f46bc69976e38c85577e15a342
Mutala Mutala <jonamut12.live@gmail.com>
2023-12-19 04:40:18 -0500

    Ajout de la response classe

---
+++ Library.BlazorUI/Services/Base/Response.cs
@@ -0,0 +1,17 @@
+﻿namespace Library.BlazorUI.Services.Base
+{
+    /// <summary>
+    /// Represente la response qu'on compte avoir au niveau du UI 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Response<T>
+    {
+        public string Message { get; set; }
+
+        public string ValidationsErrors { get; set; }
+
+        public bool Success { get; set; }
+
+        public T Data { get; set; }
+    }
+}
