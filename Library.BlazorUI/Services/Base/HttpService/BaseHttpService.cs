00519da904e8d6582067096bd72edee50c5e23dd
Mutala Mutala <jonamut12.live@gmail.com>
2023-12-19 04:59:03 -0500

    Mise en place de BaseHttpService pour convertir l'exception de L'API au UI

---
+++ Library.BlazorUI/Services/Base/HttpService/BaseHttpService.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Library.BlazorUI.Services.Base.Client;
+using MyNamespace;
+using System.Net;
+
+namespace Library.BlazorUI.Services.Base.HttpService
+{
+    public class BaseHttpService
+    {
+        protected IClientService _clientService;
+        protected readonly IMapper _mapper; 
+
+
+        public BaseHttpService(IClientService clientService, IMapper mapper)
+        {
+            _clientService = clientService;
+            _mapper = mapper;
+        }
+
+
+        /// <summary>
+        /// Methode permettant de Convertir l'exception envoyé à L'API 
+        /// Pour afficher les bonnes informations
+        /// </summary>
+        /// <typeparam name="Guid"></typeparam>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        protected Response<Guid> ConvertApiException<Guid>(ApiException exception)
+        {
+            Response<Guid> response = new Response<Guid>(); 
+
+            //Si nous recevons une BadRequest cela veut dire que les data envoyé à API sont incorrectes 
+            if(exception.StatusCode == (int)HttpStatusCode.BadRequest)
+            {
+                response = new Response<Guid>
+                {
+                    Message = "Invalid Data était soumis",
+                    ValidationsErrors = exception.Response,
+                    Success = false
+                }; 
+            }
+            else if(exception.StatusCode == (int)HttpStatusCode.NotFound)
+            {
+                response = new Response<Guid>
+                {
+                    Message = "Aucune donné trouvé",
+                    Success = false,
+                    ValidationsErrors = exception.Response
+                }; 
+            }else
+            {
+                response = new Response<Guid>
+                {
+                    Message = "Essayer Apres probleme interne",
+                    Success = false,
+                };
+            }
+            return response;
+        }
+        
+    }
+}
