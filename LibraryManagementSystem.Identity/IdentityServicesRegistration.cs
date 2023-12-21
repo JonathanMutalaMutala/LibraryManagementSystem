7926c54ca8089667dbb0c5c0a78d345808c931b0
Mutala Mutala <jonamut12.live@gmail.com>
2023-12-21 01:11:46 -0500

    Mise en place de ServicesRegistration

--- LibraryManagementSystem.Identity/IdentityServicesRegistration.cs
+++ LibraryManagementSystem.Identity/IdentityServicesRegistration.cs
@@ -1,6 +1,14 @@
-﻿using LibraryManagementSystem.Application.Models.Jwt;
+﻿using LibraryManagementSystem.Application.Identity;
+using LibraryManagementSystem.Application.Models.Jwt;
+using LibraryManagementSystem.Identity.DbContext;
+using LibraryManagementSystem.Identity.Models;
+using LibraryManagementSystem.Identity.Service;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,13 +21,43 @@ namespace LibraryManagementSystem.Identity
     {
         public static IServiceCollection AddIdentityServices(this IServiceCollection services,IConfiguration configuration)
         {
-            services.Configure<JwtSettings>(configuration.GetSection("JwtSettings")); 
+            services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
 
-            services.AddDbContext<H>
+            services.AddDbContext<LibraryManagementIdentityDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("LibraryDatabaseConnectionString"));
+            });
+            services
+                .AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<LibraryManagementIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+
+            services.AddTransient<IAuthService, AuthService>();
+
+            // Je n'accepte que le JWT comme systeme d'authentification pour Mon API
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+                    ValidIssuer = configuration["JwtSettings:Issuer"],
+                    ValidAudience = configuration["JwtSettings:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:key"]))
+                };
+            });
+
 
 
             return services; 
         }
     }
 }
-J
