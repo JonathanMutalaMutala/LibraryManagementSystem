2fcdaacc30565bd0ee559a90a00dd5afe738c6b6
Mutala Mutala <jonamut12.live@gmail.com>
2023-12-21 01:00:30 -0500

    IdentityRegistration and DbContext

---
+++ LibraryManagementSystem.Identity/DbContext/LibraryManagementIdentityDbContext.cs
@@ -0,0 +1,23 @@
+﻿using LibraryManagementSystem.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Identity.DbContext
+{
+    public  class LibraryManagementIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public LibraryManagementIdentityDbContext(DbContextOptions<LibraryManagementIdentityDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(LibraryManagementIdentityDbContext).Assembly);
+        }
+
+    }
+}
---
+++ LibraryManagementSystem.Identity/IdentityServicesRegistration.cs
@@ -0,0 +1,25 @@
+﻿using LibraryManagementSystem.Application.Models.Jwt;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Identity
+{
+    public static class IdentityServicesRegistration
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.Configure<JwtSettings>(configuration.GetSection("JwtSettings")); 
+
+            services.AddDbContext<H>
+
+
+            return services; 
+        }
+    }
+}
+J
