e4d85e8b1f8a6996cd70227927d6c56e2a0971cf
Mutala Mutala <jonamut12.live@gmail.com>
2023-12-21 22:00:38 -0500

    Mise en place de Authentification Controller

---
+++ LibraryManagementSyst.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,36 @@
+﻿using LibraryManagementSystem.Application.Identity;
+using LibraryManagementSystem.Application.Models.Login;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSyst.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        public readonly IAuthService _authenticationService; 
+
+
+        public AuthenticationController(IAuthService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        /// <summary>
+        /// Ce  Http permet de connecter un utilisateur 
+        /// Prend L'objet AuthRequest en parametre et appel la methode permettant d'authentifié l'utilisateur 
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResponse>> Login(AuthRequest request)
+        {
+            return Ok(await _authenticationService.Login(request));
+        }
+
+
+
+
+    }
+}
